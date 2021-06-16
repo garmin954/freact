28f74e9793113e6117b59f9628d5252801f16d41
期颐 <1549088785@qq.com>
2021-06-16 10:37:51 +0800

    a
    Signed-off-by: 期颐 <1549088785@qq.com>

--- store/index.js
+++
@@ -1,8 +0,0 @@
-import {createStore,applyMiddleware} from 'redux'
-import {finalReducer } from './reducer'
-const defaultState = {}
-//生成store对象
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(finalReducer, defaultState, composeEnhancers);//内部会第一次调用reducer函数，得到初始state
-
-export default store
